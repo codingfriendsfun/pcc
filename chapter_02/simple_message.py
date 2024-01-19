115af13266dd39f64ef13e4af32ce1f8f02f4861
Noelle <noellelaurinriley@gmail.com>
2024-01-18 21:45:01 -0800

    Added ch 2 ex 2.1 - 2.2 in separate files

--- chapter_02/simple_message.py
+++ chapter_02/simple_message.py
@@ -1,8 +1,3 @@
 # 2-1
 message = "My cat is a gremlin."
 print(message)
-
-#2-2 
-print(message)
-message = "Now my cat is a beanie baby."
-print(message)
---
+++ chapter_02/simple_message_2.py
@@ -0,0 +1,4 @@
+message = "My cat is still a gremlin."
+print(message)
+message = "Now my cat is a beanie baby."
+print(message)
