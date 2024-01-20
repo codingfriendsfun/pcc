f2603b096df44356e6eef02eb99f54d4bf63a56e
Alexa Adams <bookwormlex@gmail.com>
2024-01-20 13:46:04 -0800

    2-11. Adding Comments

---
+++ chapter_02/2-11-a.py
@@ -0,0 +1,7 @@
+# This exercise was originally 2-6.
+# This program assigns a person's name to a variable, then uses f-strings to 
+    # interpolate the variable into another string.
+
+famous_person = "Laurel Thatcher Ulrich"
+message = f'{famous_person} once said, "Well-behaved women rarely make history."'
+print(message)
---
+++ chapter_02/2-11-b.py
@@ -0,0 +1,6 @@
+# This file was originally 2-8
+# This program assigns a file name to a variable, then uses the built-in method
+    # removesuffix to remove the file extension from the string.
+
+filename='python_notes.txt'
+print(filename.removesuffix('.txt'))
