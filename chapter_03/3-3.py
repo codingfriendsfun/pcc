98ca4cabb40c82a3a2e81e9bd776fe6ef8b143b1
Alexa Adams <bookwormlex@gmail.com>
2024-01-20 15:50:45 -0800

    3-3. Your Own List

---
+++ chapter_03/3-3.py
@@ -0,0 +1,7 @@
+# 3-3. Your Own List: a list of types of transport, printed in a message.
+
+transport = ["electric bicycle", "electric SUV"]
+message = "I would like to own a nice"
+
+print(f"{message} {transport[0]}.")
+print(f"{message} {transport[1]}.")
