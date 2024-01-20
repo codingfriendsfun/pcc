560480a786e9770f10824dd62623c89e72b746c5
Alexa Adams <bookwormlex@gmail.com>
2024-01-20 15:44:22 -0800

    3-2. Greetings

---
+++ chapter_03/3-2.py
@@ -0,0 +1,8 @@
+# 3-2. Greetings: a list of friends' names, printed in a greeting
+
+names = ["Noelle", "Larissa"]
+
+greeting = "Nice to see you"
+
+print(f"{greeting}, {names[0]}!")
+print(f"{greeting}, {names[1]}!")
