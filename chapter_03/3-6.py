ae5ed88215252027189e07b89cd5d1c5db5d994e
Alexa Adams <bookwormlex@gmail.com>
2024-01-20 16:41:23 -0800

    3-6. Shrinking Guest List

--- chapter_03/3-6.py
+++ chapter_03/3-6.py
@@ -1,6 +1,5 @@
-# 3-6. More Guests
-# a script that invites a list of guests to a dinner party, but with a bigger 
-    # table
+# 3-7. Shrinking Guest List
+# a script that invites a list of guests to a dinner party, then uninvites some
 
 guests = ["Caileigh", "Noelle", "Larissa"]
 invite = "you are cordially invited to dinner."
@@ -23,3 +22,24 @@ print(f"{guests[2]}, {invite}")
 print(f"{guests[3]}, {invite}")
 print(f"{guests[4]}, {invite}")
 print(f"{guests[5]}, {invite}")
+
+# oops, table delivery snafu
+print("Unfortunately, the table won't be here in time. Let's reschedule.")
+
+# uninvite people
+message = "I apologize, we'll have to have dinner another time"
+print(f"{message}, {guests.pop(0)}.")
+print(f"{message}, {guests.pop()}.")
+print(f"{message}, {guests.pop()}.")
+print(f"{message}, {guests.pop()}.")
+
+# re-invite the remaining folk
+invite = "we can still have dinner."
+print(f"{guests[0]}, {invite}")
+print(f"{guests[1]}, {invite}")
+
+# empty the list
+del guests[0]
+del guests[0]
+# could have also done guests = [], but this is what book asked for
+print(guests)
