43d94b61d1ecfd92b2fe3141074f66079ed508a8
Alexa Adams <bookwormlex@gmail.com>
2024-01-20 16:19:30 -0800

    3-4. Guest List

---
+++ chapter_03/3-4.py
@@ -0,0 +1,9 @@
+# 3-4. Guest List
+# a script that invites a list of guests to a dinner party
+
+guests = ["Caileigh", "Noelle", "Larissa"]
+invite = "you are cordially invited to dinner."
+
+print(f"{guests[0]}, {invite}")
+print(f"{guests[1]}, {invite}")
+print(f"{guests[2]}, {invite}")
