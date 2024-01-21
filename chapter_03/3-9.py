c66835086a718207a10a5862d8aa1e04ae6e3b09
Alexa Adams <bookwormlex@gmail.com>
2024-01-20 17:08:23 -0800

    3-9. Dinner Guests

---
+++ chapter_03/3-9.py
@@ -0,0 +1,12 @@
+# 3-9. Dinner Guests
+# a script that invites a list of guests to a dinner party and tells you
+    # how many people you invited
+
+guests = ["Caileigh", "Noelle", "Larissa"]
+invite = "you are cordially invited to dinner."
+
+print(f"{guests[0]}, {invite}")
+print(f"{guests[1]}, {invite}")
+print(f"{guests[2]}, {invite}")
+
+print(f"You invited {len(guests)} guests to dinner.")
