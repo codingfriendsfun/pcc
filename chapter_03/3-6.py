28c69a7e266b2e8ce03e287bf51b1f155cf37bdc
Noelle <noellelaurinriley@gmail.com>
2024-01-23 15:36:27 -0800

    Added more guests

---
+++ chapter_03/3-6.py
@@ -0,0 +1,18 @@
+#Invite each guest to dinner
+guests = ['tom hiddleston', 'david tennant', 'maggie smith']
+print(f"Dear {guests[0].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[1].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[2].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+#Let them know I found a bigger table
+print("I found a bigger table!")
+#add more cool people
+guests.insert(0, 'sir ian mckellen')
+guests.insert(2, 'dame judy dench')
+guests.append('catherine tate')
+#invite all the cool people
+print(f"Dear {guests[0].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[1].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[2].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[3].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[4].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[5].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
