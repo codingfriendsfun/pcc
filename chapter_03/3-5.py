5bd6f5694f627d9190b15f7c3706e7e30daede2b
Noelle <noellelaurinriley@gmail.com>
2024-01-23 15:23:13 -0800

    Added changing guest list

---
+++ chapter_03/3-5.py
@@ -0,0 +1,11 @@
+#Invite each guest to dinner
+guests = ['tom hiddleston', 'david tennant', 'dame maggie smith']
+print(f"Dear {guests[0].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[1].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[2].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+#David Tennant cannot make it :(
+print(guests[1].title())
+guests [1] = 'sir ian mckellen'
+print(f"Dear {guests[0].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[1].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[2].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
