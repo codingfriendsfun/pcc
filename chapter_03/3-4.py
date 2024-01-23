08cbf3c9017ff459daddd5b3b5490e54c0f8a022
Noelle <noellelaurinriley@gmail.com>
2024-01-23 15:22:12 -0800

    Added guest list exercise

---
+++ chapter_03/3-4.py
@@ -0,0 +1,5 @@
+#Invite each guest to dinner
+guests = ['tom hiddleston', 'david tennant', 'maggie smith']
+print(f"Dear {guests[0].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[1].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
+print(f"Dear {guests[2].title()}, you are invited dinner to talk about acting and Shakespeare and stuff.")
