bba7b26a7902c72a624d48dadc82b2f7e982a501
Noelle <noellelaurinriley@gmail.com>
2024-01-23 16:00:59 -0800

    Added completed exercises up to 3-7

--- chapter_03/3-7.py
+++ chapter_03/3-7.py
@@ -16,3 +16,20 @@ print(f"Dear {guests[2].title()}, you are invited to dinner with even more peopl
 print(f"Dear {guests[3].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
 print(f"Dear {guests[4].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
 print(f"Dear {guests[5].title()}, you are invited to dinner with even more people to talk about acting and Shakespeare and stuff.")
+#whoops can only invite 2
+print("Unfortunately, I've been informed that only two of you may attend. My bad.")
+#send most embarrassing messages of my life
+popped_guests = guests.pop()
+print(f"Dear {popped_guests.title()}, unfortunately we have to rescind the invitation to dinner and Shakespeare and stuff. We apologize for the inconvenience.")
+popped_guests = guests.pop(2)
+print(f"Dear {popped_guests.title()}, unfortunately we have to rescind the invitation to dinner and Shakespeare and stuff. We apologize for the inconvenience.")
+popped_guests = guests.pop(0)
+print(f"Dear {popped_guests.title()}, unfortunately we have to rescind the invitation to dinner and Shakespeare and stuff. We apologize for the inconvenience.")
+popped_guests = guests.pop(1)
+print(f"Dear {popped_guests.title()}, unfortunately we have to rescind the invitation to dinner and Shakespeare and stuff. We apologize for the inconvenience.")
+#reprint invitations
+print(f"Dear {guests[0].title()}, congratulations! You are still invited to dinner.")
+print(f"Dear {guests[1].title()}, congratulations! You are still invited to dinner.")
+del guests[0]
+del guests[0]
+print(guests)
