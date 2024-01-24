0bacbbc5ec70a564a8f69c85e5f2dd89d5a2d4a8
Noelle <noellelaurinriley@gmail.com>
2024-01-23 17:15:32 -0800

    Added exercises 3-8 and 3-9

---
+++ chapter_03/3-8.py
@@ -0,0 +1,23 @@
+#list of places I'd like to visit
+places = ['Yellowstone National Park', 'Australia', "Edinburgh", "London", "Alaska"]
+print(places)
+#temp sort alphabetically
+print(sorted(places))
+#prove it was temporary
+print(places)
+#temp sort reverse alphabetically
+print(sorted(places, reverse=True))
+#prove it was temporary again
+print(places)
+#reverse the list
+places.reverse()
+print(places)
+#reverse it back
+places.reverse()
+print(places)
+#sort it alphabetically
+places.sort()
+print(places)
+#sort it reverse alphabetically
+places.sort(reverse=True)
+print(places)
