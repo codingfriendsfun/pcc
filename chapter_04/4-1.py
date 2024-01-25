6c476ca20851d8ac8ed951492c8cda5ddbbbae40
Noelle <noellelaurinriley@gmail.com>
2024-01-24 18:22:11 -0800

    Added exercises 4.1 and 4.2

---
+++ chapter_04/4-1.py
@@ -0,0 +1,7 @@
+#list three favorite pizzas
+pizzas = ['pepperoni', 'spinach and feta', 'cheese']
+#use a for loop to make a statement about it
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+#make a statement about how much I like pizza after loop
+print("Pizza is alright.")
---
+++ chapter_04/4-2.py
@@ -0,0 +1,4 @@
+big_cats = ['cheetah', 'snow leopard', 'mountain lion']
+for cat in big_cats:
+    print(f"A {cat} is a big cat.")
+print("These are some of my favorite big cats.")
