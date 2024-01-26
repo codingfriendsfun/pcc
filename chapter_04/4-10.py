3957fa7c1fbc85ba4976ddbb8c32235057514cb2
Noelle <noellelaurinriley@gmail.com>
2024-01-26 15:21:01 -0800

    Added exercise 4-10 using the list of odd numbers to pull slices

---
+++ chapter_04/4-10.py
@@ -0,0 +1,12 @@
+#Make a list of odd numbers from 1 to 20, print using a for loop
+odd_numbers = list(range(1,20,2))
+print(odd_numbers)
+#Print the first three items in the list
+print("The first three items in the list are:")
+print(odd_numbers[:3])
+#Print three items from the middle of the list
+print("Three items from the middle of the list are:")
+print(odd_numbers[4:7])
+#Print the last three items
+print("The last three items in the list are:")
+print(odd_numbers[-3:])
