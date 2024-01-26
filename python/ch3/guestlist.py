c131db5a9b3fc322b095e2cff78e2b8343da2c32
therawv <therawv@gmail.com>
2024-01-25 19:11:16 -0800

    Add files via upload

---
+++ python/ch3/guestlist.py
@@ -0,0 +1,4 @@
+names = ["George Washington", "Monkey D Luffy", "Darth Vader"]
+print("Welcome "+ names[0] + " to dinner.")
+print("Welcome "+ names[1] + " to dinner.")
+print("Welcome "+ names[2] + " to dinner.")
---
+++ python/ch3/names.py
@@ -0,0 +1,5 @@
+names = ["Bob", "Monkey D Luffy", "Zoro", "Darth Vader"]
+print("One of my friends is named "+ names[0])
+print("Another my friends is named "+ names[1])
+print("Another my friends is named "+ names[2])
+print("Another my friends is named "+ names[3])
