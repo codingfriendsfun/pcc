92a6cfa2042566962b061236053178e75233492a
Kristin <knhbjr@gmail.com>
2024-01-25 22:26:11 -0600

    my first commit

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+message = "Hello Python world!"
+print(message)
+
+mesage = "Hello Python Crash Course world!"
+print(mesage)
+
+      
---
+++ strip_whitespace.py
@@ -0,0 +1,7 @@
+# To ensure that no whitespace exists 
+#at the right side of a string, use 
+#the rstrip() method
+favorite_language = 'python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language)
