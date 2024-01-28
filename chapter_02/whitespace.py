5e332eb2761639ef9d7e90063e9e847c8f046680
Kristin <knhbjr@gmail.com>
2024-01-27 18:08:46 -0600

    Added chapter_02 folder

---
+++ chapter_02/whitespace.py
@@ -0,0 +1,9 @@
+#tab = "\t"
+print("Python")
+print("\tPython")
+
+#newline = "\n"
+print("Languages:\nPython\nC\nJavaScript")
+
+#"\n\t" = start a new line with a tab
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
