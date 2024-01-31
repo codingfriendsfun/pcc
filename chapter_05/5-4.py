8fe931693273d373824eb70eff0e90d319a05af8
Noelle <noellelaurinriley@gmail.com>
2024-01-30 16:35:04 -0800

    Added 5-4, Alien Colors #2

---
+++ chapter_05/5-4.py
@@ -0,0 +1,16 @@
+#choose alien color
+alien_color = 'red'
+
+#earn 5 points if green, 10 if other color (run else block)
+if alien_color == 'green':
+    print("You earned 5 points!")
+else:
+    print("You earned 10 points!")
+
+#change alien_color to run if block
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You earned 5 points!")
+else:
+    print("You earned 10 points!")
