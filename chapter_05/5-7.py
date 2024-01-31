8b82f394230f4359cf8c0347798f7ce191a51e30
Noelle <noellelaurinriley@gmail.com>
2024-01-30 16:36:01 -0800

    Added 5-7, Favorite Fruit

---
+++ chapter_05/5-7.py
@@ -0,0 +1,13 @@
+#list favorite fruits
+favorite_fruits = ['banana', 'kiwi', 'blueberry', 'strawberry']
+
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+if 'kiwi' in favorite_fruits:
+    print("You really like kiwi!")
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+if 'blueberry' in favorite_fruits: 
+    print("You really like blueberry!")
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
