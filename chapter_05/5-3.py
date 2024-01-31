7040edee44f8f79ff014fbc6f0d046cd875baf05
Noelle <noellelaurinriley@gmail.com>
2024-01-30 16:34:42 -0800

    Added 5-3.py, Alien Colors #1

---
+++ chapter_05/5-3.py
@@ -0,0 +1,10 @@
+#alien just shot down, assign color
+alien_color = 'green'
+
+#if green, gain 5 points
+if alien_color == 'green':
+    print("You earned 5 points!")
+
+alien_color = 'red'
+if alien_color == 'green':
+    print("You earned 5 points!")
