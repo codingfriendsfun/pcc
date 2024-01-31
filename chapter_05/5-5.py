6ccd0f9e6c20a2c235363190a7ba579640ae108f
Noelle <noellelaurinriley@gmail.com>
2024-01-30 16:35:23 -0800

    Added 5-5, Alien Colors #3

---
+++ chapter_05/5-5.py
@@ -0,0 +1,24 @@
+#write if-elif-else chain for alien colors
+alien_color = 'red'
+if alien_color == 'green':
+    print('You earned 5 points!')
+elif alien_color == 'yellow':
+    print('You earned 10 points!')
+else:
+    print("You earned 15 points!")
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('You earned 5 points!')
+elif alien_color == 'yellow':
+    print('You earned 10 points!')
+else:
+    print("You earned 15 points!")
+
+alien_color = 'green'
+if alien_color == 'green':
+    print('You earned 5 points!')
+elif alien_color == 'yellow':
+    print('You earned 10 points!')
+else:
+    print("You earned 15 points!")
