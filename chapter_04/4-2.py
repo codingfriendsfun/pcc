f196b26cc3c0ef83bd74ac96d7dfa7a44bac68a7
Alexa Adams <bookwormlex@gmail.com>
2024-01-31 19:20:46 -0800

    4-2. Animals

---
+++ chapter_04/4-2.py
@@ -0,0 +1,8 @@
+# a script that lists animals that have a trait in common.
+
+animals = ["lions", "tigers", "bears"]
+
+for animal in animals:
+    print(f"{animal.title()} are wild animals.")
+
+print("But they're also a line in a song.")
