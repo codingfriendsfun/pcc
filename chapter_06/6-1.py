d3ccb846891f8c9bddbf45939cf656530c810bb0
Noelle <noellelaurinriley@gmail.com>
2024-02-01 12:22:01 -0800

    Added 6-1, a dictionary about a person.

---
+++ chapter_06/6-1.py
@@ -0,0 +1,10 @@
+# Store information about a person you know
+person = {
+    'first_name': 'Lizzy',
+    'last_name': 'Kowalik',
+    'age': 31,
+    'city': 'Brookline'
+    }
+
+# Print each piece of information stored in your directory.
+print(person)
