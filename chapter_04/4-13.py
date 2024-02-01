b20a8f72902c51900e761c6961b74fa71a05c1c5
Alexa Adams <bookwormlex@gmail.com>
2024-01-31 21:56:41 -0800

    4-13. Buffet

---
+++ chapter_04/4-13.py
@@ -0,0 +1,14 @@
+# tuple of basic foods
+
+foods = ("steamed veggies", "rice", "beans", "hot dogs", "hamburgers")
+
+for food in foods:
+    print(food)
+
+# foods[3] = "cheddar" # doesn't work"
+print() # for readability
+
+foods = ("steamed veggies", "rice", "beans", "burritos", "tacos")
+
+for food in foods:
+    print(food)
