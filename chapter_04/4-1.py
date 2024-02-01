60a06f28b5254e3cd7cbed657a766146ff8b4bec
Alexa Adams <bookwormlex@gmail.com>
2024-01-31 19:11:54 -0800

    4-1. Pizzas

---
+++ chapter_04/4-1.py
@@ -0,0 +1,8 @@
+# a script that lists pizzas
+
+pizzas = ["Everest", "Hawaiian", "Chicken Artichoke"]
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("But I'm open to trying any pizza once.")
