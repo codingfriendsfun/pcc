352d2f2c5019be7fac42b0f5af450041bef7b1b2
Alexa Adams <bookwormlex@gmail.com>
2024-02-03 09:55:20 -0800

    6-3. Glossary

---
+++ chapter_06/6-3.py
@@ -0,0 +1,12 @@
+# Glossary
+
+glossary = {
+    'loop' : 'a way of repeating code',
+    'string' : 'a variable type used to store ASCII characters',
+    'dictionary' : 'a list where the indices (called keys) are named',
+    'operator' : 'One of the arithmetic or conditional symbols, such as "<", "or", etc',
+    'variable' : 'a way to store and access a value'
+}
+
+for key in glossary:
+    print(f"{key}: {glossary[key]}.")
