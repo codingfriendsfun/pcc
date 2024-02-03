b0454961e8dde91e1c499e1e949d230931ed8713
Alexa Adams <bookwormlex@gmail.com>
2024-02-02 19:29:36 -0800

    5-3. Alien Colors #1

---
+++ chapter_05/5-3.py
@@ -0,0 +1,13 @@
+# alien colors
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You just earned 5 points")
+
+
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("You just earned 5 points")
