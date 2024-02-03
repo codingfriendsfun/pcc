d7452931c0cb610753014bfc8680aa30e5fedd5e
Alexa Adams <bookwormlex@gmail.com>
2024-02-02 19:16:13 -0800

    5-2. More Conditional Tests

---
+++ chapter_05/5-2.py
@@ -0,0 +1,26 @@
+# more conditionals practice
+
+print("\nStrings:")
+print(f"true != false: {'true' != 'false'}")
+print(f"true == false: {'true' == 'false'}")
+
+print("\nLower():")
+print(f"True != True.lower(): {'True' != 'True'.lower()}")
+
+print("\nNumerical Equality:")
+print(f"3 == 4: {3 == 4}")
+print(f"3 != 4: {3 != 4}")
+
+print("\n> / < / >= / <= :")
+print(f"3 < 4: {3 < 4}")
+print(f"3 > 4: {3 > 4}")
+print(f"3 <= 4: {3 <= 4}")
+print(f"3 >= 4: {3 >= 4}")
+
+print("\nAND / OR :")
+print(f"True and False: {True and False}")
+print(f"True or False: {True or False}")
+
+print("\nin or not in [list] :")
+print(f"1 in range(1,3): {1 in range(1,3)}")
+print(f"3 in range(1,3): {3 in range(1,3)}")
