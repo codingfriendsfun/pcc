7d2f931af4850ea00dba41c5a5886154d8684a90
Alexa Adams <bookwormlex@gmail.com>
2024-02-03 09:47:24 -0800

    6-2. Favorite Numbers

---
+++ chapter_06/6-2.py
@@ -0,0 +1,12 @@
+# favorite numbers
+
+fav_num = {
+    'Alexa' : 7,
+    'Ariel' : 4,
+    'Caileigh' : 13,
+    'Oliver' : 6,
+    'Wednesday' : 3
+}
+
+for key in fav_num:
+    print(f"{key}'s favorite number is {fav_num[key]}.")
