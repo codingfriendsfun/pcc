9239d3a740b38ff929935d4316587ca6b15925a4
Alexa Adams <bookwormlex@gmail.com>
2024-02-03 14:20:45 -0800

    6-9. Favorite Places

---
+++ chapter_06/6-9.py
@@ -0,0 +1,10 @@
+# 
+
+favorite_places = {
+    'Alexa' : ['Forest', 'Beach'],
+    'Caileigh' : 'Beach',
+    'James' : 'Disney'
+}
+
+for person in favorite_places:
+    print(f"{person}'s favorite places: {favorite_places[person]}")
