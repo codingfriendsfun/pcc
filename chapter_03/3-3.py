9a72c163ab0518bb2825c1b44bb0d048547f97d0
Kristin <knhbjr@gmail.com>
2024-02-04 17:18:29 -0600

    Completed exercise 3-3

---
+++ chapter_03/3-3.py
@@ -0,0 +1,5 @@
+make = ['Honda', 'Mazda', 'Ford']
+model = ['Miata', 'Mustang', 'S2000']
+print(f'I would like to own a {make[0]} {model[2]} someday.')
+print(f'I would like to own a {make[1]} {model[0]} someday.')
+print(f'I would like to own a {make[2]} {model[1]} someday.')
