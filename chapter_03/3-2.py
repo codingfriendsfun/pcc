6d387d66e2b1fef2e0e84fc210e5eb9d38a397c3
Kristin <knhbjr@gmail.com>
2024-02-04 17:13:25 -0600

    Completed exercise 3-2

---
+++ chapter_03/3-2.py
@@ -0,0 +1,5 @@
+names = ['Irma', 'Ellie', 'Elijah', 'Pa']
+print(f'{names[0]} is the grandmother of my children.')
+print(f'{names[1]} is my daughter.')
+print(f'{names[2]} is my son.')
+print(f'{names[3]} is the grandfather of my children.')
