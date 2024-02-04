3ab440a1c8216247420ad786b90f328e162d5f43
Kristin <knhbjr@gmail.com>
2024-02-04 14:45:14 -0600

    Added 2-7.py

---
+++ chapter_02/2-7.py
@@ -0,0 +1,14 @@
+
+name = '\tEllie\nGarcia'
+print(name)
+print("\n") 
+#new line
+print(name.lstrip())
+
+print("\n") 
+#new line
+print(name.rstrip())
+
+print("\n") 
+#new line
+print(name.strip())
