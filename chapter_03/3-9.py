6cbb6f37d6be5fa0addff20a719ad9c19607ef8e
Kristin <knhbjr@gmail.com>
2024-02-05 15:59:45 -0600

    Completed exercise 3-9 (using the list in exercise 3-4)

---
+++ chapter_03/3-9.py
@@ -0,0 +1,7 @@
+#using 3-4.py
+guest_list = ['Mom', 'Ellie', 'Elijah']
+print(f"You're invited to dinner, {guest_list[0]}!")
+print(f"Come on, {guest_list[1]}, you don't get a choice to join dinner!")
+print(f"{guest_list[2]}, yes you're invited and yes, you must eat the food.")
+#exercise 3-9
+print(f"\nThis makes a total of {len(guest_list)} guests joining us.")
