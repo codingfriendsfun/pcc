6e80b1de78b00a4bb02d79d13a3790bdadc86bf7
Noelle <noellelaurinriley@gmail.com>
2024-02-04 21:08:41 -0800

    Edited 4-11 so that second half prints after a new line

--- chapter_04/4-11.py
+++ chapter_04/4-11.py
@@ -14,6 +14,6 @@ friend_pizzas.append('hawaiian')
 print("My favorite pizzas are:")
 for pizza in pizzas:
     print(pizza)
-print("My friend's favorite pizzas are:")
+print("\nMy friend's favorite pizzas are:")
 for pizza in friend_pizzas:
     print(pizza)
