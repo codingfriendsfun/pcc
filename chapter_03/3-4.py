4d0805e9fe8215d71ccac1a79985518c51eed00b
Kristin <knhbjr@gmail.com>
2024-02-04 20:07:25 -0600

    Completed exercise 3-4

---
+++ chapter_03/3-4.py
@@ -0,0 +1,4 @@
+guest_list = ['Mom', 'Ellie', 'Elijah']
+print(f"You're invited to dinner, {guest_list[0]}!")
+print(f"Come on, {guest_list[1]}, you don't get a choice to join dinner!")
+print(f"{guest_list[2]}, yes you're invited and yes, you must eat the food.")
