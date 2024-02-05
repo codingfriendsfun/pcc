d7c198bbc660c77102c93921a2e818bfe7c976a5
Noelle <noellelaurinriley@gmail.com>
2024-02-04 20:42:45 -0800

    Added exercise 5-10, but a version that actually works

--- chapter_05/5-10.py
+++ chapter_05/5-10.py
@@ -3,13 +3,9 @@ current_users = ['kerowyn', 'suntanna', 'scriptless', 'kristin', 'eclipse']
 #list 5 new_users
 new_users = ['Suntanna', 'cmpsr2000', 'dt777', 'caileigh', 'xorphix']
 
-for user in current_users:
-    current_users.append(user)
 for user in new_users:
-    new_users.append(user)
-#loop through new_users to see if username has already been used
-for new_user in new_users:
-    if new_user in current_users:
-        print(f"The username {new_user} is taken. Please try a different username.")
+    user = user.lower()
+    if user in current_users:
+        print(f"The username {user} is taken. Please try a different username.")
     else:
         print("That username is available!")
