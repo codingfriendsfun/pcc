9533ed3e8c05ba458b764e650536e8cc2fafad63
Kristin <knhbjr@gmail.com>
2024-02-06 17:39:43 -0600

    Completed exercise 4-11

---
+++ chapter_04/4-11.py
@@ -0,0 +1,10 @@
+pizzas = ['pepperoni', 'black olives', 'cheese']
+friend_pizzas = pizzas[:]
+pizzas.append('mushroom')
+friend_pizzas.append('supreme')
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza.title())
+print("\nMy friends favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza.title())
