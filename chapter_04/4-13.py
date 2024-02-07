ebf1dcc9f8f297e3b5b1b8d9bb3cfe6fc9dc879b
Kristin <knhbjr@gmail.com>
2024-02-06 18:02:10 -0600

    Completed exercise 4-13

---
+++ chapter_04/4-13.py
@@ -0,0 +1,9 @@
+buffet_food = ('steak', 'spaghetti', 'baked chicken', 'alfredo', 'meatloaf')
+print("We offer the following entrees:")
+for food in buffet_food:
+    print(food.title())
+
+buffet_food = ('ribs', 'porkchops', 'baked chicken', 'alfredo', 'meatloaf')
+print("\nEnjoy our updated menu:")
+for food in buffet_food:
+    print(food.title())
