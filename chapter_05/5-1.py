a0c8075b17f4217cb6b4a12aa2d125a21dfb43db
Kristin <knhbjr@gmail.com>
2024-02-06 21:09:09 -0600

    Completed exercise 5-1

---
+++ chapter_05/5-1.py
@@ -0,0 +1,32 @@
+game_title = 'Eqevo'
+print(f"{game_title} is stored in game_title.\n")
+
+print("Is game_title == 'Eqevo'? I predict True")
+print(game_title == 'Eqevo')
+
+print("\nIs game_title.title() == 'Eqevo'? I predict True")
+print(game_title.title() == 'Eqevo')
+
+print("\nIs game_title.lower() == 'eqevo'? I predict True")
+print(game_title.lower() == 'eqevo')
+
+print("\nIs game_title.upper() == 'EQEVO'? I predict True")
+print(game_title.upper() == 'EQEVO')
+
+print("\nIs game_title != 'eqevo'? I predict True")
+print(game_title != 'eqevo')
+
+print("\nIs game_title == 'eqevo'? I predict False")
+print(game_title == 'eqevo')
+
+print("\nIs game_title.upper() == 'eqevo'? I predict False")
+print(game_title.upper() == 'eqevo')
+
+print("\nIs game_title.title() == 'eqevo'? I predict False")
+print(game_title.title() == 'eqevo')
+
+print("\nIs game_title.strip() == 'eqevo'? I predict False")
+print(game_title.strip() == 'eqevo')
+
+print("\nIs game_title != 'Eqevo'? I predict False")
+print(game_title != 'Eqevo')
