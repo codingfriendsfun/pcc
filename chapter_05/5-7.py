d98b67e0381327d6b9ffe35a803c819b40253dab
Kristin <knhbjr@gmail.com>
2024-02-07 02:07:02 -0600

    Completed 5-7

---
+++ chapter_05/5-7.py
@@ -0,0 +1,11 @@
+favorite_fruit = ['watermelon', 'apple', 'banana']
+if 'watermelon' in favorite_fruit:
+    print('I like watermelon\n')
+if 'apple' in favorite_fruit:
+    print('I like apple\n')
+if 'pineapple' in favorite_fruit:
+    print('i like pineapple\n')
+if 'banana' in favorite_fruit:
+    print('I like banana')
+if 'cherry' in favorite_fruit:
+    print('i like cherry')
