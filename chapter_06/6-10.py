84f114deae1c8c946bb2098c00ffa17f6ac7e487
Noelle <noellelaurinriley@gmail.com>
2024-02-06 17:09:34 -0800

    Added 6-10, favorite numbers

---
+++ chapter_06/6-10.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+    'noelle': [7, 3],
+    'kristin': [5, 8],
+    'alexa': [7, 9],
+    'ravi': [4, 8],
+    'larissa': [6, 28]
+    }
+
+for person in favorite_numbers:
+    print(f"{person.title()}'s favorite numbers are {favorite_numbers[person]}")
