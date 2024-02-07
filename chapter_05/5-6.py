b118c7e03d1ae6a01272c3d38abf7923746f6fb2
Kristin <knhbjr@gmail.com>
2024-02-07 02:02:13 -0600

    Completed 5-6

---
+++ chapter_05/5-6.py
@@ -0,0 +1,15 @@
+age = 12
+
+if age < 2:
+    print('baby')
+elif age < 4:
+    print('toddler')
+elif age < 13:
+    print('kid')
+elif age < 20:
+    print('teenager')
+elif age < 65:
+    print('adult')
+elif age >= 65:
+    print('elder')
+
