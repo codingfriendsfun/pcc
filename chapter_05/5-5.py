4e74e827da547b872cd6f6f887584afc6b97ab36
Kristin <knhbjr@gmail.com>
2024-02-07 01:54:52 -0600

    Completed exercise 5-5

---
+++ chapter_05/5-5.py
@@ -0,0 +1,29 @@
+alien_color = 'red'
+if alien_color == 'green':
+    points = 5
+elif alien_color == 'yellow':
+    points = 10
+elif alien_color == 'red':
+    points = 15
+
+print(f"Your score is {points}")
+
+alien_color_2 = 'green'
+
+if alien_color_2 == 'green':
+    points = 5
+elif alien_color_2 == 'yellow':
+    points = 10
+elif alien_color_2 == 'red':
+    points = 15
+
+print(f"\nYour score is {points}")
+
+alien_color_3 = 'yellow'
+if alien_color_3 == 'green':
+    points = 5
+elif alien_color_3 == 'yellow':
+    points = 10
+elif alien_color_3 == 'red':
+    points = 15
+print(f"\nYour score is {points}")
