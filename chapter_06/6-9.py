24463a0662f6f0ffb202bb7762ad34344330f969
Noelle <noellelaurinriley@gmail.com>
2024-02-06 17:09:19 -0800

    Added 6-9, favorite places

---
+++ chapter_06/6-9.py
@@ -0,0 +1,8 @@
+favorite_places = {
+    '10-year-old me': 'colorado',
+    '12-year-old me': 'edinburgh, scotland',
+    '20-year-old me': 'yellowstone national park',
+    }
+
+for key, value in favorite_places.items():
+    print(f"\n{key}'s favorite place was {value.title()}.")
