ce9f547b594a9bda62ba4acd972921a76048de6e
Kristin <knhbjr@gmail.com>
2024-02-07 01:47:52 -0600

    Completed 5-4

---
+++ chapter_05/5-4.py
@@ -0,0 +1,12 @@
+alien_color = 'red'
+#else
+if alien_color == 'green':
+    print("You've earned 5 points.")
+else:
+     print("You've earned 10 points.")
+
+#if
+if alien_color == 'red':
+    print("You've earned 5 points.")
+else:
+     print("You've earned 10 points.")
