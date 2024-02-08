5733b49aa7bf4b99694380f9107ee63b5290e6a3
Noelle <noellelaurinriley@gmail.com>
2024-02-07 22:18:39 -0800

    Added 7-2, automated hostess

---
+++ chapter_07/7-2.py
@@ -0,0 +1,7 @@
+dinner_people = input("How many people will be dining tonight? ")
+dinner_people = int(dinner_people)
+
+if dinner_people > 8:
+    print("There will be a brief wait for your table.")
+else:
+    print("Your table is ready! I'd say to follow me, but I'm a computer")
