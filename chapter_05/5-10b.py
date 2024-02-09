9cc232ab06730c14543e2d95b16c6988f5c0ba55
Alexa Adams <bookwormlex@gmail.com>
2024-02-09 07:48:07 -0800

    a more compact solution

---
+++ chapter_05/5-10b.py
@@ -0,0 +1,10 @@
+# website username simulation
+
+current_users = ["name1", "name2", "namE3", "admin", "name4"]
+new_users = ["name5", "NAME2", "name6", "Name3", "name7"]
+
+for new in [user.lower() for user in new_users]:
+    if new in [user.lower() for user in current_users]:
+        print(f"Username {new} taken, please choose another.")
+    else:
+        print(f"Username {new} available.")
