8a31c7261453b0afe7ff71fb9bc3036e3dbddb42
Kristin <knhbjr@gmail.com>
2024-02-08 20:48:47 -0600

    Did further testing, nothing was actually changed

--- chapter_05/5-10.py
+++ chapter_05/5-10.py
@@ -1,7 +1,6 @@
 current_users = ['uSeR', 'ReSu', 'admin', 'name', 'eman']
 new_users = ['Kero', 'hens', 'AdMiN', 'nAMe', 'idfk']
-
-#make current users lowercase to compare
+#make current users lowercase t
 current_users_lower = [user.lower() for user in current_users]
 
 for user in new_users:
