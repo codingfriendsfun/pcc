82b32cbc20b0fd0c4ac0050b560ba696cc7fd912
Noelle <noellelaurinriley@gmail.com>
2024-02-09 19:20:03 -0800

    Added 7-7, infinite loop

---
+++ chapter_07/7-7.py
@@ -0,0 +1,7 @@
+
+
+while True:
+    print("This is a loop that never ends.")
+    print("Yes it goes on and on my friends!")
+    print("Some people started running it not knowing what it was.")
+    print("And they'll continue running it forever just because")
