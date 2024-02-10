889a87b6d62be5b92497198ae9c9a28da405f2b8
Kristin <knhbjr@gmail.com>
2024-02-10 17:05:25 -0600

    completed exercise 6-2

---
+++ chapter_06/6-2.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'kris': 13,
+    'eli': 7,
+    'dobs': 3,
+    'blake': 11,
+    'zack': 8,
+    }
+
+print(f"Kris' favorite numbers is {favorite_numbers['kris']}")
+print(f"Eli's favorite numbers is {favorite_numbers['eli']}")
+print(f"Dobs' favorite numbers is {favorite_numbers['dobs']}")
+print(f"Blake's favorite numbers is {favorite_numbers['blake']}")
+print(f"Zack's favorite numbers is {favorite_numbers['zack']}")
