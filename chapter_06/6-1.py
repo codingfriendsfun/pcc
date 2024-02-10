5bc23d594bf22a22d4e0d88d6b37f9bcc0ed1280
git config --global user.name [Ravinder <therawv@gmail.com>
2024-02-10 12:32:36 -0800

    starting ch6

---
+++ chapter_06/6-1.py
@@ -0,0 +1,10 @@
+person_1 = {
+    'first': 'Ravi',
+    'last': 'Ghotra',
+    'age': 36,
+    'city': "Long Beach"
+}
+
+for key, value in person_1.items():
+    print (f"\nKey: {key.title()}")
+    print (f"Value: {value}")
---
+++ chapter_06/6-3.py
@@ -0,0 +1,10 @@
+glossary = {
+    'print': 'outputs the things in () can use f to allow more complex outputs',
+    'if': 'checks conditions and outputs true or false',
+    'elif': 'if the first condition fails it checks for another condition',
+    'else': 'if the other conditions fail it runs this',
+    'for': 'does something for a time'
+}
+
+for key, value in glossary.items():
+    print (f"\ncommand: {key.title()}, function: {value}")
