cf1d75d921255aac40385494253a04b726ac3ddd
Kristin <knhbjr@gmail.com>
2024-02-10 16:57:30 -0600

    completed exercise 6-1

---
+++ chapter_06/6-1.py
@@ -0,0 +1,14 @@
+#dictionary = alien_0
+#key = 'color'
+#value = 'green'
+ellie_bio = {
+    'first_name': 'ellie',
+    'last_name': 'garcia',
+    'age': 0,
+    'home_town': 'grand prairie',
+    }
+
+print(ellie_bio['age'])
+print(ellie_bio['first_name'].title())
+print(ellie_bio['home_town'].title())
+print(ellie_bio['last_name'].title())
