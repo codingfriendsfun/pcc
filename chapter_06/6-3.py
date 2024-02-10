0387df44086ab0c5ee4e6d95f7c5b785e1f13d4b
Kristin <knhbjr@gmail.com>
2024-02-10 17:15:21 -0600

    completed exercise 6-3

---
+++ chapter_06/6-3.py
@@ -0,0 +1,23 @@
+#index
+index = {
+    'list': 'An array.',
+    'title()': 'Capitalizes the first letters of each word.',
+    'upper()': 'Converts all letters to uppercase.',
+    'lower()': 'Converts all letters to lowercase.',
+    'strip()': 'Removes beginning and trailing whitespace.',
+    }
+
+print('List:')
+print(f"\t{index['list']}")
+
+print('\ntitle():')
+print(f"\t{index['title()']}")
+
+print('\nupper():')
+print(f"\t{index['upper()']}")
+
+print('\nlower():')
+print(f"\t{index['lower()']}")
+
+print('\nstrip():')
+print(f"\t{index['strip()']}")
