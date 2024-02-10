f35a847ed1b05fdfd2873fb2a14a2a922700f4c9
git config --global user.name [Ravinder <therawv@gmail.com>
2024-02-10 12:48:43 -0800

    6-5

---
+++ chapter_06/6-5.py
@@ -0,0 +1,8 @@
+rivers = {
+    'nile': 'africa',
+    'colorado': 'north america',
+    'yellow': 'asia'
+}
+
+for key, value in rivers.items():
+    print (f"\nCountry: {key.title()}, River: {value.title()}")
