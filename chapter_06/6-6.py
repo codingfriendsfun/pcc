6032d962ae6ea0740d7202ee336e1a6abe254c50
git config --global user.name [Ravinder <therawv@gmail.com>
2024-02-10 16:04:32 -0800

    6-6

---
+++ chapter_06/6-6.py
@@ -0,0 +1,15 @@
+favorite_languages = {
+    'jen': 'python',
+    'Rob': 'html',
+    'chris': 'rust',
+    'larissa': 'java',
+    'ravi': 'python'
+}
+
+pollees = ["Ravi", "rob", "Lauren", "noelle", "kristin"]
+
+for poll in pollees:
+    if poll.lower() in [favorite.lower() for favorite in favorite_languages]:
+        print("You've already been polled!")
+    else:
+        print("You need to take our poll")
