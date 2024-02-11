03acae5eb444eb5ba4ab0d339d02ff2c37fc76d0
git config --global user.name [Ravinder <therawv@gmail.com>
2024-02-10 16:53:50 -0800

    6-10

---
+++ chapter_06/6-10.py
@@ -0,0 +1,10 @@
+favoriteNumbersList = {
+    'ravi': [8,88],
+    'dylon': 100,
+    'bob': 1,
+    'george': 3,
+    'kevin': 7,
+}
+
+for key, value in favoriteNumbersList.items():
+    print (f"\nPerson: {key.title()}, Favorite Number: {value}")
