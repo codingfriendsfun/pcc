d44d43ffe2cf60ff787af17227215c84b9b53032
git config --global user.name [Ravinder <therawv@gmail.com>
2024-02-10 16:45:13 -0800

    6-7

---
+++ chapter_06/6-7.py
@@ -0,0 +1,27 @@
+person_1 = {
+    'first': 'Ravi',
+    'last': 'Ghotra',
+    'age': 36,
+    'city': "Long Beach"
+}
+
+person_2 = {
+    'first': 'Dan',
+    'last': 'Bautista',
+    'age': 35,
+    'city': "Anaheim"
+}
+
+person_3 = {
+    'first': 'Chris',
+    'last': 'Richardson',
+    'age': 'classified',
+    'city': "classified"
+}
+
+people = [person_1, person_2, person_3]
+
+for person in people:
+    for key, value in person.items():
+        print (f"\nKey: {key.title()}")
+        print (f"Value: {value}")
