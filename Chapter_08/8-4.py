63bab50c756d2df37fdac371833b985d62decc45
Noelle <noellelaurinriley@gmail.com>
2024-02-12 18:10:23 -0800

    Added 8-4, large shirts

---
+++ Chapter_08/8-4.py
@@ -0,0 +1,6 @@
+def make_shirt(size='large', text='I love Python'):
+    print(f"You ordered a size {size} shirt that says '{text}'.")
+
+make_shirt()
+make_shirt('medium')
+make_shirt(text='Chaos and Rainbows', size='small')
