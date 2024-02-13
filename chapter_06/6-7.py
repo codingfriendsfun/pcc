14d7aa2668440d22dcfe6445888dab0fd8d04c88
Kristin <knhbjr@gmail.com>
2024-02-13 02:21:02 -0600

    completed 6-7

---
+++ chapter_06/6-7.py
@@ -0,0 +1,26 @@
+#6-1
+ellie = {
+    'first': 'ellie',
+    'last': 'garcia',
+    'age': 0,
+    'town': 'grand prairie',
+    }
+#new
+elijah = {
+    'first': 'elijah',
+    'last': 'garcia',
+    'age': 7,
+    'town': 'grand prairie',
+}
+
+kristin = {
+    'first': 'kristin',
+    'last': 'hensley',
+    'age': 27,
+    'town': 'jacksonville',
+}
+
+people = [ellie, elijah, kristin]
+
+for person in people:
+    print(person)
