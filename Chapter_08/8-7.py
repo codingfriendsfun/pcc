e630b37a9a0b282cedada316403354c458903d61
Noelle <noellelaurinriley@gmail.com>
2024-02-13 14:35:44 -0800

    Added 8-7, album exercise for PR

--- Chapter_08/8-7.py
+++ Chapter_08/8-7.py
@@ -1,1 +1,18 @@
-test
+def make_album(artist_name, album_title, songs=None):
+    """Stores album information in a dictionary"""
+    album = {
+        'arist': artist_name,
+        'album': album_title,
+    }
+    if songs:
+        album['songs'] = songs
+    return album
+
+album = make_album('Boris Slavov', "Baldur's Gate 3 Original Soundtrack",)
+print(album)
+
+album = make_album('Olivia Rodrigo', 'guts', 12)
+print(album)
+
+album = make_album('Noah Kahan', 'Stick Season')
+print(album)
