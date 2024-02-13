156d3670200da6f790927c9a5c89c7744aa96f4f
Noelle <noellelaurinriley@gmail.com>
2024-02-12 18:07:53 -0800

    Added 8-3, T-shirt

---
+++ Chapter_08/8-3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, text):
+    print(f"You ordered a size {size} shirt that says '{text}'.")
+
+make_shirt('medium', 'chaos and rainbows')
+
+make_shirt(text='peace was never an option', size='medium')
