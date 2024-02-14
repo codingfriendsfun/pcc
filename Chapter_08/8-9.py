a4207145a813194e1c32c8309d4a03d626192335
Noelle <noellelaurinriley@gmail.com>
2024-02-14 12:32:50 -0800

    Added 8-9, Messages

---
+++ Chapter_08/8-9.py
@@ -0,0 +1,7 @@
+def show_messages(text_messages):
+    """Print a text message"""
+    for text_message in text_messages:
+        print(text_message)
+
+text_messages = ['lmao', 'kk', 'wtf should I write', "I don't feel creative"]
+show_messages(text_messages)
