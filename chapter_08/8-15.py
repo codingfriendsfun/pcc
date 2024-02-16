865cd93f19fa83e28c45f1ca09dbf398e181b0b4
Noelle <noellelaurinriley@gmail.com>
2024-02-15 23:57:29 -0800

    Completed 8-15.py, printing models

--- chapter_08/8-15.py
+++ chapter_08/8-15.py
@@ -1,1 +1,8 @@
-#printing models
+import printing_functions
+
+# List unprinted designs and create blank list for completed models
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+printing_functions.print_models(unprinted_designs, completed_models)
+printing_functions.show_completed_models(completed_models)
