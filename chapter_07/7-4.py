7e5599ab458d34cd838bd447fb3e8b384b61c55e
Alexa Adams <bookwormlex@gmail.com>
2024-02-17 17:43:21 -0800

    fixing line ordering issue in 7-4

--- chapter_07/7-4.py
+++ chapter_07/7-4.py
@@ -6,5 +6,5 @@ while topping != 'quit':
     if topping == 'quit':
         break
     else:
-        print(f"I'll add {topping} to your pizza.\n")
         topping = input("What topping would you like on your pizza? ")
+        print(f"I'll add {topping} to your pizza.\n")
