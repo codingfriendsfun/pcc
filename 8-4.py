ddd8b3b9af83d2e0990014cfb3da23dd765f87bd
Alexa Adams <bookwormlex@gmail.com>
2024-02-17 20:33:05 -0800

    8-4 Large Shirts

---
+++ 8-4.py
@@ -0,0 +1,8 @@
+# 8-4 Large Shirts
+
+def make_shirt(size="large", message="I love Python"):
+    print(f"Your {size} shirt will say: '{message}'")
+
+make_shirt()
+make_shirt(size="medium")
+make_shirt(message="What a tshirt, eh?")
