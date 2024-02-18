a87399660c5c9c2395ad2c6a3815d2dbd52d1874
Alexa Adams <bookwormlex@gmail.com>
2024-02-18 13:47:45 -0800

    8-10. Sending Messages

---
+++ 8-10.py
@@ -0,0 +1,24 @@
+# 8-10. Sending Messages
+
+messages = [
+    "On my way!",
+    "Running a bit late.",
+    "Do you believe in love after love?"
+]
+
+sent_messages = []
+
+def show_messages(message_list):
+    for message in message_list:
+        print(message)
+
+def send_messages(message_list):
+    while message_list:
+        popped_message = message_list.pop()
+        print(popped_message)
+        sent_messages.append(popped_message)
+
+send_messages(messages)
+
+print(messages)
+print(sent_messages)
