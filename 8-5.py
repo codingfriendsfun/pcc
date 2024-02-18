91d3e3d40c69715ef7d6a2193e1791a52858b694
Alexa Adams <bookwormlex@gmail.com>
2024-02-17 20:45:33 -0800

    8-5. Cities

---
+++ 8-5.py
@@ -0,0 +1,8 @@
+# 8-5. Cities
+
+def describe_city(city, country="England"):
+    print(f"{city.title()} is in {country.title()}.")
+
+describe_city("London")
+describe_city("Seattle", "United States")
+describe_city("Tokyo", "Japan")
