d3ea55dd49b59bea08d073590952aa447eb06fe4
Alexa Adams <bookwormlex@gmail.com>
2024-02-17 21:17:45 -0800

    8-6. City Names

---
+++ 8-6.py
@@ -0,0 +1,9 @@
+# 8-6. City Names
+
+def city_country(city, country):
+    return (f"{city.title()}, {country.title()}")
+
+
+print(city_country("Seattle", "United States"))
+print(city_country("Tokyo", "Japan"))
+print(city_country("Santiago", "Chile"))
