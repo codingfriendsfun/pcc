50d5f610ae9e8adbdb8cd86cd5d10915fce1a90e
Alexa Adams <bookwormlex@gmail.com>
2024-02-18 13:32:53 -0800

    8-9. Messages

---
+++ 8-9.py
@@ -0,0 +1,13 @@
+# 8-9. Messages
+
+messages = [
+    "On my way!",
+    "Running a bit late.",
+    "Do you believe in love after love?"
+]
+
+def show_messages(message_list):
+    for message in message_list:
+        print(message)
+
+show_messages(messages)
