a57c85962682b63dfde3eb9ccde6fa612da2bcdb
Alexa Adams <bookwormlex@gmail.com>
2024-02-18 16:11:26 -0800

    8-12. Sandwiches

---
+++ chapter_08/8-12.py
@@ -0,0 +1,8 @@
+# 8-12. Sandwiches
+
+def sandwich(*ingredients):
+    print(f"Sandwich contents: {ingredients}")
+
+sandwich("pastrami", "cheddar", "horseradish", "pickles")
+sandwich("pastrami", "cheddar", "horseradish")
+sandwich("pastrami", "cheddar")
