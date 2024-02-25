cfa0a6cc804d36c3fe6439dea86e5db322fec9f2
Noelle <noellelaurinriley@gmail.com>
2024-02-24 22:10:45 -0800

    Added version of 9-8 that I need help with

---
+++ chapter_09/9-8.py
@@ -0,0 +1,52 @@
+class User:
+    """A basic class containing user profile informaiton."""
+
+    def __init__(self, first_name, last_name):
+        """Initialize first and last name attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def describe_user(self):
+        """Print a summary of the information collected about the user."""
+        print(f"First name: {self.first_name.title()}")
+        print(f"Last name: {self.last_name.title()}")
+    
+    def greet_user(self):
+        """Print a customized greeting to the user."""
+        full_name = f"{self.first_name.title()} {self.last_name.title()}"
+        print(f"Hello, {full_name}.")
+
+class Privileges:
+    """Represents the different privileges an account"""
+
+    def __init__(self, privileges=[]):
+        """Initialize attributes"""
+        self.privileges = privileges
+
+    def show_privileges(self):
+        """Show the privileges an admin has."""
+        print("A user of your rank can do the following:")
+        for privilege in self.privileges:
+            print(f"{privilege}")
+
+class Admin(User):
+    """Represent specific traits of the admin type of user."""
+
+    def __init__(self, first_name, last_name):
+        """
+        Initialize attributes of parent class (user).
+        Initialize attributes of admin.
+        """
+        super().__init__(first_name, last_name)
+        self.privileges = Privileges()
+
+admin_0 = Admin('yoda', 'adoy', )
+admin_0.describe_user()
+
+admin_0.privileges = [
+    'can grant rank of master',
+    'can grant seat on council',
+    'can declare sith lord'
+]
+
+admin_0.privileges.show_privileges()
