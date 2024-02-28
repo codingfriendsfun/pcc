340eee11be23fa5e6fbae8503ac8d4ab7dd6d9fb
Kristin <knhbjr@gmail.com>
2024-02-27 19:15:16 -0600

    Completed exercise 7-7

---
+++ chapter_07/7-7.py
@@ -0,0 +1,12 @@
+prompt = 'Type "play" to activate a fun song!\n'
+sing = input(prompt)
+
+active = True
+while active:
+    if sing == "play":
+        print("This is the song that doesn't end")
+        print("Yes, it goes on and on, my friend")
+        print("Some people started singing it not knowing what it was,")
+        print("And they′ll continue singing it forever just because)\n")
+    else:
+        break
