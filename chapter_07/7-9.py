032d19bd7e391b348eda8633f85bc3d5cc045e4c
Kristin <knhbjr@gmail.com>
2024-02-28 17:40:35 -0600

    Updated file 7-9.py

--- chapter_07/7-9.py
+++ chapter_07/7-9.py
@@ -1,6 +1,10 @@
 sandwich_orders = ['tuna', 'pastrami','pastrami', 'reuben', 'italian', 'dogfish', 'pastrami']
 finished_sandwiches = []
 
+#solution that was asked for, but I prefer the if/else solution
+#print("Sorry, we have run out of pastrami.")
+#while 'pastrami' in sandwich_orders:
+#    sandwich_orders.remove('pastrami')
 while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
     if current_sandwich == 'pastrami':
