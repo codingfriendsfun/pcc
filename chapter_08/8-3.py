f1c39342fb7292c1a937f931071867c4760b0239
Kristin <knhbjr@gmail.com>
2024-02-28 17:37:52 -0600

    Completed exercise 8-3

---
+++ chapter_08/8-3.py
@@ -0,0 +1,5 @@
+def make_shirt(size, message):
+    print(f'Making a size {size} shirt with "{message}" written on it.')
+
+make_shirt(8, 'Some cheesy phrase.')
+make_shirt(message='Phrase cheesy some', size=15)
