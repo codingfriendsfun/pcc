acda066b28e64c6d403891559e73d23e2118b741
Kristin <knhbjr@gmail.com>
2024-02-28 18:00:38 -0600

    Completed exercise 8-4

---
+++ chapter_08/8-4.py
@@ -0,0 +1,6 @@
+def make_shirt(size='large', message='I love Python!'):
+    print(f'Making a size {size} shirt with "{message}" written on it.')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Python love I!')
