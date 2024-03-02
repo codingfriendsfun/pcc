806206915eed57d0aa255b728b23ce82f9f7df34
Kristin <knhbjr@gmail.com>
2024-03-01 19:07:28 -0600

    Completed exercise 8-11

---
+++ chapter_08/8-11.py
@@ -0,0 +1,31 @@
+#working from 8-10 code
+
+#function that prints messages stored in a list
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+#moves unsent messages to sent
+def send_messages(unsent_messages, sent_messages):
+    while unsent_messages:
+        sending_message = unsent_messages.pop()
+        print(sending_message)
+        sent_messages.append(sending_message)
+
+#list of sent and unsent messages
+unsent_messages = [
+    'I am a text message in this program', 
+    'How are you today, text message?',
+    'Hi text message, it is warm today.']
+sent_messages = []
+
+#verify messages are in unsent_messages and sent_messages is empty
+print(unsent_messages)
+print(sent_messages)
+
+#pass copy of unsent_messages to keep values
+send_messages(unsent_messages[:], sent_messages)
+
+#verify unsent_messages hasn't had values removed
+print(unsent_messages)
+print(sent_messages)
