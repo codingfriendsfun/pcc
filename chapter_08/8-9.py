a30022e97625b4b9476f629b29b0e438cdf548ea
Kristin <knhbjr@gmail.com>
2024-03-01 18:54:14 -0600

    Completed exercise 8-9

---
+++ chapter_08/8-9.py
@@ -0,0 +1,10 @@
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+messages = [
+    'I am a text message in this program', 
+    'How are you today, text message?',
+    'Hi text message, it is warm today.']
+
+show_messages(messages)
