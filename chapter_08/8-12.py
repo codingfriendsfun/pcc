7d452f0312e18e38c06a3c092e3cdb05466cd75c
Kristin <knhbjr@gmail.com>
2024-03-02 23:23:51 -0600

    Added a missing docstring to a function in  8-12.py

--- chapter_08/8-12.py
+++ chapter_08/8-12.py
@@ -1,5 +1,6 @@
 #items on a sandwich
 def sandwich_toppings(*toppings):
+    """Lists the toppings added to a sandwich."""
     print("\nAdding the following toppings to your sandwich:")
     for topping in toppings:
         print(f"- {topping}")
