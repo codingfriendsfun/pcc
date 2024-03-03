8ea77f2d7b3fd8a17185380f6153a5baf42bafd1
Kristin <knhbjr@gmail.com>
2024-03-02 23:12:31 -0600

    Adding a missing docstring to 8-6.py and added descriptive commments to explain the code.

--- chapter_08/8-6.py
+++ chapter_08/8-6.py
@@ -1,10 +1,14 @@
 def city_country(name, country):
+    """Formats a city, country pair."""
     city_country_formatted = f"{name.title()}, {country.title()}"
     return city_country_formatted
 
+# using city_country() to describe cities
 dallas = city_country('dallas', 'america')
 london = city_country('london', 'england')
 vancouver = city_country('vancouver', 'canada')
+
+#verifies the function and variables are correct
 print(dallas)
 print(london)
 print(vancouver)
