9b708bfac1fa47e01b3ec2da5f84b5cb1e313767
Kristin <knhbjr@gmail.com>
2024-03-02 23:09:35 -0600

    Added a missing docstring to the function in 8-3.py

--- chapter_08/8-3.py
+++ chapter_08/8-3.py
@@ -1,4 +1,5 @@
 def make_shirt(size, message):
+    """Display the size and message printed on an ordered t-shirt."""
     print(f'Making a size {size} shirt with "{message}" written on it.')
 
 make_shirt(8, 'Some cheesy phrase.')
