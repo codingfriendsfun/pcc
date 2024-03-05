70e0534ff7478e15aa510c8ad3c553a3d1583d5c
Noelle <noellelaurinriley@gmail.com>
2024-03-04 17:20:50 -0800

    Added 10-5, guest book with relevant text file

---
+++ chapter_10/10-5.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+prompt = "What is your name? "
+prompt += "Enter q to quit. "
+
+guest_book = []
+name = ""
+
+# while loop for users to input their names
+while name != "q":
+    name = input(f"{prompt}")
+    if name == "q":
+        break
+    guest_book.append(name)
+
+# Establish path and variable to be written to path
+path = Path('./chapter_10/guest.txt')
+guests = ''
+
+# Add guest names to variable as a string
+for guest in guest_book:
+    guests += f"{guest} "
+    
+# Write to file
+path.write_text(guests)
