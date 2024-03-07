ecc7c8c9ef1c1f8dcc71682b053d5f9da300995e
Restyled.io <commits@restyled.io>
2024-03-07 21:26:03 +0000

    Restyled by black

--- chapter_08/8-14.py
+++ chapter_08/8-14.py
@@ -1,18 +1,14 @@
 # model and manufacturer are required
 def car_details(model, manufacturer, **car_info):
     """Build a dictionary containing details about a car"""
-    car_info['model'] = model
-    car_info['manufacturer'] = manufacturer
+    car_info["model"] = model
+    car_info["manufacturer"] = manufacturer
     return car_info
 
 
 # creating 2 cars using the function
-car_1 = car_details('miata', 'mazda',
-                    convertible=True,
-                    color='white')
-car_2 = car_details('accord', 'honda',
-                    color='blue',
-                    luxury_trim=True)
+car_1 = car_details("miata", "mazda", convertible=True, color="white")
+car_2 = car_details("accord", "honda", color="blue", luxury_trim=True)
 
 # verifies the function worked to store all key-value pairs
 print(car_1)
