41a1be781eda44bbe9656f2ee58e029010964644
Restyled.io <commits@restyled.io>
2024-03-07 21:27:23 +0000

    Restyled by autopep8

--- chapter_08/8-7.py
+++ chapter_08/8-7.py
@@ -1,12 +1,13 @@
 def make_album(artist_name, artist_title, song_number=None):
     """Return dictionaries containing album info"""
-    album = {'name': artist_name, 'title': artist_title.title(),}
+    album = {'name': artist_name, 'title': artist_title.title(), }
     if song_number:
         album['song_number'] = song_number
-    
+
     return album
 
-#create albums using make_album()
+
+# create albums using make_album()
 bop = make_album('MGK', 'lace up')
 flop = make_album('MGK', 'binge')
 semi_bop = make_album('MGK', 'tickets to my downfall', song_number=15)
