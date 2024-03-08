58f8a13fb2b2eb49931a23380cc5fbb00d35bddf
git config --global user.name [Ravinder <therawv@gmail.com>
2024-03-07 20:02:46 -0800

    more stuff

---
+++ chapter_07/7-9.py
@@ -0,0 +1,27 @@
+sandwich_orders = ["BLT", "Turkey Club", "Veggie Delight", "Pastrami", "Grilled Cheese", "Pastrami", "Pastrami", "Pastrami"]
+
+# Check if there's enough pastrami
+if sandwich_orders.count("Pastrami") >= 3:
+    print("Apologies, the deli has run out of pastrami.")
+    while "Pastrami" in sandwich_orders:
+        # Remove all occurrences of pastrami from sandwich orders
+        sandwich_orders.remove("Pastrami")
+else:
+    print("Pastrami is available! Let's make some sandwiches.")
+
+# Initialize an empty list for finished sandwiches
+finished_sandwiches = []
+
+
+
+
+# Loop through each sandwich order
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)  # Get the first sandwich order
+    print(f"I made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+# Print a message listing each finished sandwich
+print("\nFinished sandwiches:")
+for sandwich in finished_sandwiches:
+    print(sandwich)
---
+++ chapter_08/8-1.py
@@ -0,0 +1,4 @@
+def display_message():
+    print("We are learning how to make functions(with inputs) and call them.")
+
+display_message()
