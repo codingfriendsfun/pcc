cd054bf98dab8a53562eacdcf50d96c55815fa90
git config --global user.name [Ravinder <therawv@gmail.com>
2024-03-11 21:39:12 -0700

    8-7 redone

--- chapter_08/8-7.py
+++ chapter_08/8-7.py
@@ -1,11 +1,11 @@
-def make_album(artist, album):
+def make_album(artist, album, songs=1):
     """create a dictionary with an arist and an album"""
-    new_Album = {'Artist': artist.title(), 'Album':album.title()}
+    new_Album = {'Artist': artist.title(), 'Album':album.title(), 'Songs':songs}
     return new_Album
 
-LincolnPark = make_album('Lincoln Park', 'The End')
-Tupac = make_album('Tupac', 'Me against the world')
-Beastie = make_album('Beastie Boys', 'Licensed to ill')
+LincolnPark = make_album('Lincoln Park', 'Solo EP')
+Tupac = make_album('Tupac', 'Me against the world', 15)
+Beastie = make_album('Daft Punk', 'Da Funk', 3)
 
 print(LincolnPark)
 print(Tupac)
