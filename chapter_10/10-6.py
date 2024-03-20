f54dec0bb55fdac191cb5f4819c8ce87ad8207e4
Noelle <noellelaurinriley@gmail.com>
2024-03-20 15:29:44 -0700

    Removed the while loop for 10-6 so that 10-7 can be correct

--- chapter_10/10-6.py
+++ chapter_10/10-6.py
@@ -1,17 +1,11 @@
 print("Too lazy to add things yourself? Don't worry, I gotchu.")
-print("(Enter 'q' to quit.)")
 
-while True:
-    first_number = input("Enter the first number: ")
-    if first_number == 'q':
-        break
-    second_number = input("Enter the second number: ")
-    if second_number == 'q':
-        break
-    try:
-        sum = int(first_number) + int(second_number)
-    except ValueError:
-        print("That is neither the number nor the way to quit. Try again :)")
-        # The smile makes it a friendly error message
-    else:
-        print(sum)
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+try:
+    sum = int(first_number) + int(second_number)
+except ValueError:
+    print("One of those is not a number :)")
+    # The smile makes it a friendly error message
+else:
+    print(sum)
