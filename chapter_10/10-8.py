99cc1a11fbdb5766921a179b1bf9dc64f534887f
Noelle <noellelaurinriley@gmail.com>
2024-03-20 15:51:56 -0700

    Added 10-8, cats and dogs, and necessary text files

---
+++ chapter_10/10-8.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+pet_names = ['./chapter_10/cats.txt', './chapter_10/dogs.txt']
+
+for pet_name in pet_names:
+    try:
+        path = Path(pet_name)
+        pet_name = path.read_text()
+    
+        print("Some good pet names are:")
+        print(pet_name)
+    except FileNotFoundError:
+        print("Oops, we're missing some pet names :')")
