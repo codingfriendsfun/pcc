f40f4e168a1608dcff78963f9d595ada1b5c3cec
Kerowyn202 <157073915+Kerowyn202@users.noreply.github.com>
2024-03-21 22:11:21 -0700

    Update 8-7, added blank lines around album

--- Chapter_08/8-7.py
+++ Chapter_08/8-7.py
@@ -1,9 +1,11 @@
 def make_album(artist_name, album_title, songs=None):
     """Stores album information in a dictionary"""
+    
     album = {
         'arist': artist_name,
         'album': album_title,
     }
+    
     if songs:
         album['songs'] = songs
     return album
