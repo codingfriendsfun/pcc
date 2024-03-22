8ad8026092217dc4f44dc9f10b592da29b8f3422
git config --global user.name [Ravinder <therawv@gmail.com>
2024-03-21 18:52:48 -0700

    8-9

---
+++ chapter_08/8-9.py
@@ -0,0 +1,11 @@
+# Define a list of short text messages
+messages = ["Hello, how are you?", "Good morning!", "Have a great day!", "See you soon!"]
+
+# Function show messages
+def show_message(messages):
+    for message in messages:
+        print(message)
+
+# Call the function to show messages
+show_message(messages)
+
