1d0e22224126c3422939f5303897aac80fa61f84
Restyled.io <commits@restyled.io>
2024-03-22 05:11:25 +0000

    Restyled by autopep8

--- Chapter_08/8-7.py
+++ Chapter_08/8-7.py
@@ -1,14 +1,15 @@
 def make_album(artist_name, album_title, songs=None):
     """Stores album information in a dictionary"""
-    
+
     album = {
         'arist': artist_name,
         'album': album_title,
     }
-    
+
     if songs:
         album['songs'] = songs
     return album
+
 
 print(make_album('Boris Slavov', "Baldur's Gate 3 Original Soundtrack",))
 
