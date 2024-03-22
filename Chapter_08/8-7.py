b2e28a2ebf27783eb40dde198c3586432f305f9a
Restyled.io <commits@restyled.io>
2024-03-22 05:11:26 +0000

    Restyled by black

--- Chapter_08/8-7.py
+++ Chapter_08/8-7.py
@@ -2,20 +2,25 @@ def make_album(artist_name, album_title, songs=None):
     """Stores album information in a dictionary"""
 
     album = {
-        'arist': artist_name,
-        'album': album_title,
+        "arist": artist_name,
+        "album": album_title,
     }
 
     if songs:
-        album['songs'] = songs
+        album["songs"] = songs
     return album
 
 
-print(make_album('Boris Slavov', "Baldur's Gate 3 Original Soundtrack",))
+print(
+    make_album(
+        "Boris Slavov",
+        "Baldur's Gate 3 Original Soundtrack",
+    )
+)
 
 
-album = make_album('Olivia Rodrigo', 'guts', 12)
+album = make_album("Olivia Rodrigo", "guts", 12)
 print(album)
 
-album = make_album('Noah Kahan', 'Stick Season')
+album = make_album("Noah Kahan", "Stick Season")
 print(album)
