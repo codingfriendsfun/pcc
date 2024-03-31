3368bd6c9581d436fbbaa4dc80ed11164e4800a9
git config --global user.name [Ravinder <therawv@gmail.com>
2024-03-31 11:43:26 -0700

    9-8

---
+++ chapter_09/9-8.py
@@ -0,0 +1,30 @@
+class User:
+    def __init__(self, first, last, email, phone):
+        self.first_name = first
+        self.last_name = last
+        self.email = email
+        self.phone = phone
+        
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name}, {self.email}, {self.phone}")
+    
+    def greet_user(self):
+        print(f"Hello {self.first_name}")
+
+class Admin(User):
+    def __init__(self, first, last, email, phone, privlidges =[]):
+        super().__init__(first, last, email, phone)
+        self.userPrivlidge = Privlidges(privlidges)
+
+class Privlidges:
+    def __init__(self, privlidges =[]):
+        self.my_privlidge = privlidges
+
+    def show_privlidge(self):
+        for privli in self.my_privlidge:
+            print(f"Has the ability to: {privli}")
+
+adminPower = ['Ban Users', 'Refund Orders', 'Make Schedules', 'Talk Back']
+Ravi = Admin('Ravi', 'Ghotra', 'therawv@gmail.com','5622902248', adminPower)
+
+Ravi.userPrivlidge.show_privlidge()
