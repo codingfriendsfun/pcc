b6b54259cebba3e03c9f4c4c75f859e517cae904
git config --global user.name [Ravinder <therawv@gmail.com>
2024-03-31 11:44:43 -0700

    9-13

---
+++ chapter_09/9-13.py
@@ -0,0 +1,19 @@
+from random import randint
+
+class Die:
+    def __init__(self, sides=6):
+        self.sides = sides
+    
+    def roll_die(self):
+        return randint(1,self.sides)
+
+six = Die()
+ten = Die(10)
+twenty = Die(20)
+
+for _ in range(10):
+    print(six.roll_die())
+for _ in range(10):
+    print(ten.roll_die())
+for _ in range(10):
+    print(twenty.roll_die())
