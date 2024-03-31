686145f0c4918a5658a4aa5b81b873a11b741d4d
git config --global user.name [Ravinder <therawv@gmail.com>
2024-03-31 11:44:23 -0700

    9-11

---
+++ chapter_09/9-11.py
@@ -0,0 +1,6 @@
+from admin import Admin as Ad
+
+adminPower = ['Ban Users', 'Refund Orders', 'Make Schedules', 'Talk Back']
+Ravi = Ad('Ravi', 'Ghotra', 'therawv@gmail.com','5622902248', adminPower)
+
+Ravi.userPrivlidge.show_privlidge()
