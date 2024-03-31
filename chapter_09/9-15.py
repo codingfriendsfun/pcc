f8c02ccbe37d048f3ea34fea54d9e270a411485b
git config --global user.name [Ravinder <therawv@gmail.com>
2024-03-31 13:01:54 -0700

    9-15 more cleanup

--- chapter_09/9-15.py
+++ chapter_09/9-15.py
@@ -6,15 +6,16 @@ while winner == False:
     possible_pulls = [1,2,3,4,5,6,7,8,9,10,'r','g','a','v','i']
     my_ticket = [1,7,8,'a']
     winning_numbers =[]
-    
 
     for i in range(4):
         winning_numbers.append(choice(possible_pulls))
     if my_ticket == winning_numbers:
         print('You win!')
-        print(f'It took {count} trys to win. Your ticket {my_ticket}, and the winning numbers {winning_numbers}')
+        print(f'It took {count} trys to win. Your ticket {my_ticket}, ' \
+              f'and the winning numbers {winning_numbers}')
         winner = True
     else:
-        print(f'You Lost, winning ticket was {winning_numbers} and your ticket was {my_ticket}')
+        print(f'You Lost, winning ticket was {winning_numbers} and your '\
+              f'ticket was {my_ticket}')
         count += 1
         print(count)
