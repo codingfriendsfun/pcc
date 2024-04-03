969716255b058e4e2e6d53f069d3ee9151768f6c
Kerowyn202 <noellelaurinriley@gmail.com>
2024-04-03 16:21:39 -0700

    Added 12-1, blue sky

---
+++ chapter_12/12-1.py
@@ -0,0 +1,36 @@
+import pygame
+import sys
+
+class BlueSky: 
+    """Class to make a Pygame screen with a blue background"""
+    
+    def __init__(self):
+        """Initialize the game and create resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Blue Sky")
+
+        # Set bg color
+        self.bg_color = (135, 206, 235)
+    
+    def run_game(self):
+        """Start loop for game, enable quit"""
+        while True: 
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw screen during each pass through loop
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, run the game
+    bs = BlueSky()
+    bs.run_game()
