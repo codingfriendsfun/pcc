7ed40a8cd9ca93161a1320b92563460ed782365e
Kristin <knhbjr@gmail.com>
2024-04-03 19:18:08 -0500

    Completed exercise 10-2

---
+++ chapter_10/10-2.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+path = Path('text_files/learning_python.txt')
+
+contents = path.read_text()
+contents = contents.replace('Python', 'C')
+
+lines = contents.splitlines()
+py_string = ''
+
+for line in lines:
+    py_string += f"{line} \n"
+
+print(contents)
+print("\n")
+print(lines)
+print("\n")
+print(py_string)
