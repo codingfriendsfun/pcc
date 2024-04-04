e55d85f5270f9bd078850ae78ab6c757a2420604
Restyled.io <commits@restyled.io>
2024-04-04 13:36:43 +0000

    Restyled by pyment

--- chapter_10/10-8.py
+++ chapter_10/10-8.py
@@ -12,6 +12,12 @@ if index > 0:
 
 
 def readCatFile(catFile="cats.txt", animal="cats"):
+    """
+
+    :param catFile:  (Default value = "cats.txt")
+    :param animal:  (Default value = "cats")
+
+    """
     file = Path(f"{cwd}/chapter_10/{catFile}")
     try:
         fileDump = file.read_text()
@@ -23,6 +29,12 @@ def readCatFile(catFile="cats.txt", animal="cats"):
 
 
 def readDogFile(dogFile="dogs.txt", animal="dogs"):
+    """
+
+    :param dogFile:  (Default value = "dogs.txt")
+    :param animal:  (Default value = "dogs")
+
+    """
     readCatFile(dogFile, animal)
 
 
