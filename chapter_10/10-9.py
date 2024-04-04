2f5fa15d8d44681c648f24ee33d48a06832fb6c4
Alexa Adams <bookwormlex@gmail.com>
2024-04-04 06:33:00 -0700

    10-9. Silent Cats and Dogs

---
+++ chapter_10/10-9.py
@@ -0,0 +1,37 @@
+# 10-9. Silent Cats and Dogs
+
+from pathlib import Path
+
+# deal with "What folder am I working from?"
+import os
+cwd = os.getcwd()
+index = cwd.find("chapter")
+if index > 0:
+    cwd = cwd[:index]
+# else do nothing, assume we're in workspace root
+
+def readCatFile(catFile='cats.txt', animal="cats"):
+    file = Path(f"{cwd}/chapter_10/{catFile}")
+    try:
+        fileDump = file.read_text()
+    except FileNotFoundError:
+        return
+    print(f"Printing list of {animal}: ")
+    print(f"{fileDump}\n")
+
+def readDogFile(dogFile='dogs.txt', animal="dogs"):
+    readCatFile(dogFile, animal)
+
+catFile = input("Enter cat file name, or enter nothing to use default cats.txt: ")
+dogFile = input("Enter dog file name, or enter nothing to use default dogs.txt: ")
+print() # output a blank line for readability
+
+if not catFile:
+    readCatFile()
+else:
+    readCatFile(catFile)
+
+if not dogFile:
+    readDogFile()
+else:
+    readDogFile(dogFile)
