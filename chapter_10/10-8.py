ca92dd7dbc9f04aa8de292c75ca638e8782b5c8a
git config --global user.name [Ravinder <therawv@gmail.com>
2024-04-07 12:50:02 -0700

    10-8

---
+++ chapter_10/10-8.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+files = [Path('cats.txt'), Path('dogs.txt')]
+
+for file in files:
+    try:
+        filedump = f'{file} names: \n'
+        filedump += file.read_text()
+        print(filedump)
+    except FileNotFoundError:
+        print(f"Error {file} not found")
