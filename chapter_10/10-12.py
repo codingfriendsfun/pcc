d5aaf7c430bac146e36769461b9aa7d6c933160a
Restyled.io <commits@restyled.io>
2024-04-07 21:10:42 +0000

    Restyled by yapf

--- chapter_10/10-12.py
+++ chapter_10/10-12.py
@@ -42,7 +42,8 @@ def retrieveNumber(jsonFile):
     """
     path = Path(jsonFile)
     if path.exists():
-        print("I know your favorite number! It's " + f"{json.loads(path.read_text())}.")
+        print("I know your favorite number! It's " +
+              f"{json.loads(path.read_text())}.")
     else:
         print("Cached file not found, prompting for number...")
         storeNumber(jsonFile, getNumber())
