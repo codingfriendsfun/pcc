bb4f3df9f2c0b61642dc3795a7441eec2df1a83e
Restyled.io <commits@restyled.io>
2024-04-07 21:10:40 +0000

    Restyled by pyment

--- chapter_10/10-12.py
+++ chapter_10/10-12.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 
 def getRootDir():
+    """ """
     # deal with "What folder am I working from?"
     cwd = os.getcwd()
     index = cwd.find("chapter")
@@ -17,10 +18,17 @@ def getRootDir():
 
 
 def getNumber():
+    """ """
     return int(input("Please enter your favorite number: "))
 
 
 def storeNumber(jsonFile, num):
+    """
+
+    :param jsonFile: 
+    :param num: 
+
+    """
     path = Path(jsonFile)
     path.write_text(json.dumps(num))
     print("Favorite number cached.")
@@ -28,6 +36,11 @@ def storeNumber(jsonFile, num):
 
 
 def retrieveNumber(jsonFile):
+    """
+
+    :param jsonFile: 
+
+    """
     path = Path(jsonFile)
     if path.exists():
         print("I know your favorite number! It's " + f"{json.loads(path.read_text())}.")
@@ -38,6 +51,7 @@ def retrieveNumber(jsonFile):
 
 
 def main():
+    """ """
     rootDir = getRootDir()
 
     # create scratch dir
