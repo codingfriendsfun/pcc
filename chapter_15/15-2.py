ccd6fe236103990e5df286f1fe8fd442041c676b
Alexa Adams <bookwormlex@gmail.com>
2024-04-13 23:28:30 -0700

    15-2. Colored Cubes

---
+++ chapter_15/15-2.py
@@ -0,0 +1,18 @@
+# 15-2. Colored Cubes
+
+import matplotlib.pyplot as plt
+plt.style.available
+
+input_values = range(1, 5001)
+cubes = [x**3 for x in input_values]
+
+
+fig, ax = plt.subplots()
+ax.scatter(input_values, cubes, c=cubes, cmap=plt.cm.Greens, s=10)
+
+ax.set_title("Cubed Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+ax.tick_params(labelsize=14)
+
+plt.show()
