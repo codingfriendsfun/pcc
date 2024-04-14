e5512a0098187c7ef4db1f2977fd05c030ab1148
Alexa Adams <bookwormlex@gmail.com>
2024-04-13 19:24:16 -0700

    11-3. Employee

---
+++ chapter_11/test_employee.py
@@ -0,0 +1,20 @@
+# test suite for 11-3. Employee
+# test_employee.py
+
+from employee import Employee
+import pytest
+
+@pytest.fixture
+def employee():
+    """employee to be shared by all tests"""
+    return Employee("Huckleberry", "Finn", 0)
+
+
+def test_give_default_raise(employee):
+    employee.give_raise()
+    assert employee.salary == 5000
+
+
+def test_give_custom_raise(employee):
+    employee.give_raise(10)
+    assert employee.salary == 10
