eaa62ec5839afe0f1e12d584794d20fda767f467
Kristin <knhbjr@gmail.com>
2024-04-17 22:47:47 -0500

    Testing for employee.py updated to account for an error being fixed in employee.py

--- chapter_11/test_employee.py
+++ chapter_11/test_employee.py
@@ -12,12 +12,12 @@ def new_employee():
 def test_give_default_raise(new_employee):
     """Test that the default raise is given."""
 
-    new_salary = new_employee.give_raise()
-    assert new_salary == 45000
+    new_employee.give_raise()
+    assert new_employee.salary == 45000
 
 
 def test_give_custom_raise(new_employee):
     """Test that a custom raise is given."""
 
-    new_salary = new_employee.give_raise(10000)
-    assert new_salary == 50000
+    new_employee.give_raise(10000)
+    assert new_employee.salary == 50000
