a6075dfd7753d5e1af7367b07cfd26dfbe49e337
Alexa Adams <bookwormlex@gmail.com>
2024-04-21 12:06:58 -0700

    Adding basic bullet class

---
+++ Alien_Invasion/bullet.py
@@ -0,0 +1,26 @@
+# Bullet class for Alien Invasion
+
+import pygame
+from pygame.sprite import Sprite
+from alien_logs import AlienLogger
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position."""
+
+        self.logs = AlienLogger(__name__)
+        self.logs.logger.info(f"Initializing Bullet.")
+
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Create a bullet rect at (0,0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Store the bullet's position as a float.
+        self.y = float(self.rect.y)
