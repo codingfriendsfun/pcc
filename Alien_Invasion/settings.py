065ac86a3dbd53f543ec6bab369e853433140dc7
Alexa Adams <bookwormlex@gmail.com>
2024-04-21 11:00:47 -0700

    Adding bullet settings

--- Alien_Invasion/settings.py
+++ Alien_Invasion/settings.py
@@ -10,3 +10,9 @@ class Settings:
         self.screen_width = sw
         self.screen_height = sh
         self.bg_color = bgc
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
