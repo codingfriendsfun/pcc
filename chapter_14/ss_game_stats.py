bc20a631dca41b4e5af3fae33345fef09513fe54
Kerowyn202 <noellelaurinriley@gmail.com>
2024-04-23 19:55:25 -0700

    Added and displayed high score

--- chapter_14/ss_game_stats.py
+++ chapter_14/ss_game_stats.py
@@ -7,6 +7,9 @@ class GameStats:
         self.reset_stats()
         self.alien_hits = 0
 
+        # High score should never be reset
+        self.high_score = 0
+
     
     def reset_stats(self):
         """Initialize stats that can change during game"""
--- chapter_14/ss_scoreboard.py
+++ chapter_14/ss_scoreboard.py
@@ -15,8 +15,9 @@ class Scoreboard:
         self.text_color = (105, 105, 105)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Prep initial score image
+        # Prep initial score images
         self.prep_score()
+        self.prep_high_score()
 
 
     def prep_score(self):
@@ -30,8 +31,29 @@ class Scoreboard:
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 15
+    
+
+    def prep_high_score(self):
+        """Turn high score into rendered image"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(high_score_str, True,
+            self.text_color, self.settings.border_color)
+        
+        # Center high score at top of screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
 
     def show_score(self):
         """Draw score to screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+
+    def check_high_score(self):
+        """Check for new high score"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
