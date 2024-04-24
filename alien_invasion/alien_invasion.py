87acc0c41cb255d3a2bf97c98f7919db2bd9ad21
Kristin <knhbjr@gmail.com>
2024-04-23 19:46:50 -0500

    Added a missing attribute

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -47,7 +47,7 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             
             elif event.type == pygame.KEYUP:
-                self._check_keyup_events
+                self._check_keyup_events(event)
 
     
     def _check_keydown_events(self, event):
