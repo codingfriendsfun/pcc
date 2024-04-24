708bd0fc5fbf24c927f057c7df0e59da6a44b4a0
Kristin <knhbjr@gmail.com>
2024-04-24 01:37:24 -0500

    Bullets can be fired now.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 
@@ -22,6 +23,7 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
@@ -31,6 +33,7 @@ class AlienInvasion:
 
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -59,6 +62,9 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
 
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -73,10 +79,19 @@ class AlienInvasion:
             self.ship.moving_left = False
 
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
        
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+            
         self.ship.blitme()
 
         pygame.display.flip()
