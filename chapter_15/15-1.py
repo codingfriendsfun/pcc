1b31639b7bb29e2c2879c5bd0a14fb3d4d0645d7
Kerowyn202 <noellelaurinriley@gmail.com>
2024-04-25 21:56:41 -0700

    Updated 15-1 to hold the first 5k cubes

--- chapter_15/15-1.py
+++ chapter_15/15-1.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 8, 27, 64, 125]
+#x_values = [1, 2, 3, 4, 5]
+x_values = range(1, 5000)
+y_values = [x**3 for x in x_values]
 
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
@@ -10,12 +11,12 @@ ax.scatter(x_values, y_values, s=10)
 # Set chart title; label axes
 ax.set_title("Cubes", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Cube of Value, fontsize=14")
+ax.set_ylabel("Cube of Value", fontsize=14)
 
 # Set size of tick labels
 ax.tick_params(labelsize=14)
 
 # Set range for each axis
-ax.axis([0, 6, 0, 150])
+ax.axis([0, 5100, 0, 130_000_000_000])
 
 plt.show()
