a8feebc714c4a60f03502f19719dba63dbd1e5bd
Kerowyn202 <noellelaurinriley@gmail.com>
2024-04-30 19:46:54 -0700

    Added 15-10, graphing die rolling with Matplotlib

---
+++ chapter_15/15-10a.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from die import Die
+
+plt.style.use('dark_background')
+
+# Create die
+die = Die()
+
+# Roll die
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze results
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Graph data
+fig = plt.figure()
+
+plt.bar(x=poss_results, height=frequencies)
+plt.xlabel("Results")
+plt.ylabel("Frequency of Results")
+plt.title("Results of Rolling a D6 1k Times")
+
+plt.show()
