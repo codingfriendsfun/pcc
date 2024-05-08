e951ec78b147c0b387ffdc316904bd62c2131e3b
Kristin <knhbjr@gmail.com>
2024-05-07 23:06:12 -0500

    Aliens move left and right, check if aliens hit the side of the screen.

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -24,8 +24,15 @@ class Alien(Sprite):
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
 
-    def update(self):
-        """Move the alien to the right."""
+    def check_edges(self):
+        """Return True if alian is at edge of screen."""
 
-        self.x += self.settings.alien_speed
+        screen_rect = self.screen.get_rect()
+
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        """Move the alien right or left."""
+
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
