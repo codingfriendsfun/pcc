9c9a95ba810c6c898bbd48341f96b6f5f8d514a3
Kristin <knhbjr@gmail.com>
2024-05-15 15:20:33 -0500

    Missing attributes for button rect

--- alien_invasion/button.py
+++ alien_invasion/button.py
@@ -16,6 +16,10 @@ class Button:
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
+        # Build the button's rect object and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center  
+
         # The button message needs to be prepper only once.
         self._prep_msg(msg)
 
