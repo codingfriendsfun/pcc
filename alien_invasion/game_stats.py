1a36a440ae20b323e6aed423e5f3ed9bb5d0b896
Kristin <knhbjr@gmail.com>
2024-05-16 17:29:55 -0500

    Refactored some code, edited some mistakes.

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -5,8 +5,10 @@ class GameStats:
         """Initialize statistics."""
 
         self.settings = ai_game.settings
-        self.reset_stats()
+        
         self.high_score = 0
+
+        self.reset_stats()
 
 
     def reset_stats(self):
