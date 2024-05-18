83552de5ee8ae1d7831b4deb6b8b6a1ef8b6d445
Kristin <knhbjr@gmail.com>
2024-05-18 18:14:49 -0500

    Cubes up to 5000 with colormap

---
+++ chapter_15/15-2.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+
+x_values = list(range(5001))
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn-v0_8-pastel')
+
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=x_values, cmap=plt.cm.Reds, s=10)
+
+# Set chart title and label axis
+ax.set_title("Cube numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 5100, 0, 5100**3])
+ax.ticklabel_format(style='plain')
+
+
+plt.show()
