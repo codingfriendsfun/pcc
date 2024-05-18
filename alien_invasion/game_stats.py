27c07103c02096a33523864d03b0eabf399ca46c
Kristin <knhbjr@gmail.com>
2024-05-17 21:17:22 -0500

    Added methods to save and retrieve the high score. set self.high_score to retrieve the score

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,3 +1,7 @@
+import json
+from pathlib import Path
+
+
 class GameStats:
     """Track statistics for Alien Invasion."""
 
@@ -6,7 +10,7 @@ class GameStats:
 
         self.settings = ai_game.settings
         
-        self.high_score = 0
+        self.high_score = self._get_high_score()
 
         self.reset_stats()
 
@@ -17,4 +21,28 @@ class GameStats:
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
+
+    def _get_high_score(self):
+        """Retrieve the games highest score."""
+
+        path = Path('high_score.json')
+
+        if path.exists():
+            contents = path.read_text()
+            self.high_score = int(json.loads(contents))
+
+        else:
+            self.high_score = 0
+
+        return self.high_score
+    
+
+    def save_high_score(self):
+        """Save the high score after each session."""
+
+        path = Path('high_score.json')
+
+        contents = json.dumps(self.high_score)
+        path.write_text(contents)
+
         
