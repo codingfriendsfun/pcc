41e3bfeecf1bf542fc8eec7eea77bc16ba4239a3
Kristin <knhbjr@gmail.com>
2024-05-23 22:18:50 -0500

    Changed the lists for x and y values for an interesting new path.

--- chapter_15/random_walk.py
+++ chapter_15/random_walk.py
@@ -21,11 +21,11 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go, and how far to go.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
+            x_direction = choice([1])
+            x_distance = choice([0, -1, -2, -3, -4])
             x_step = x_direction * x_distance
 
-            y_direction = choice([1, -1])
+            y_direction = choice([-1])
             y_distance = choice([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance
 
