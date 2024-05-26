5de237e363b993bfdf503c56bc5d8aa85937ceaa
Kerowyn202 <noellelaurinriley@gmail.com>
2024-05-25 23:09:39 -0700

    Created Pizza class

--- chapter_18/pizzas/models.py
+++ chapter_18/pizzas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    """A pizza"""
+    name = models.CharField(max_length=50)
+    # Debated adding date_added, but do we really care? 
+    # No
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
