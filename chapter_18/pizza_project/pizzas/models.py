36688487b2927a8b2aabf261c0bbb147556a8c73
Kerowyn202 <noellelaurinriley@gmail.com>
2024-05-26 13:22:04 -0700

    Added DateTimeField to Pizza model

--- chapter_18/pizza_project/pizzas/models.py
+++ chapter_18/pizza_project/pizzas/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Pizza(models.Model):
     """Class to detail types of pizza"""
     name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a string representation of model"""
--- chapter_18/pizza_project/pizzeria_project/settings.py
+++ chapter_18/pizza_project/pizzeria_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'pizzas',
+    
     # Default Django apps
     'django.contrib.admin',
     'django.contrib.auth',
