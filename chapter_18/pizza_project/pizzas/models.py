a4008a9972ca23085d2c0d214a10c3d435fec932
Kerowyn202 <noellelaurinriley@gmail.com>
2024-05-26 13:28:48 -0700

    Added Topping class to models

--- chapter_18/pizza_project/pizzas/models.py
+++ chapter_18/pizza_project/pizzas/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
+
 class Pizza(models.Model):
     """Class to detail types of pizza"""
     name = models.CharField(max_length=50)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """Return a string representation of model"""
-        return self.text
+        """Return a string representation of pizza"""
+        return self.name
+
+
+class Topping(models.Model):
+    """A class to detail the toppings a pizza can have"""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    topping = models.CharField(max_length=20)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return string representation of topping"""
+        return self.topping
+    
