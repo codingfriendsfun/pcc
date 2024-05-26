349754e2a5ae661987838b69223e75cf836e1a60
Kerowyn202 <noellelaurinriley@gmail.com>
2024-05-26 13:04:58 -0700

    Added Pizza model

--- chapter_18/pizza_project/pizzas/models.py
+++ chapter_18/pizza_project/pizzas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    """Class to detail types of pizza"""
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        """Return a string representation of model"""
+        return self.text
