ff0c4f42c1aa7fed92815cb3554f2ca8b65b66ef
Kerowyn202 <noellelaurinriley@gmail.com>
2024-05-25 23:18:47 -0700

    Created superuser account

--- chapter_18/pizzas/models.py
+++ chapter_18/pizzas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Pizza(models.Model):
     """A pizza"""
     name = models.CharField(max_length=50)
--- chapter_18/pizzeria_project/settings.py
+++ chapter_18/pizzeria_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # My apps
-    'pizzas',
+    'pizzeria_project',
     # Default django apps
     'django.contrib.admin',
     'django.contrib.auth',
