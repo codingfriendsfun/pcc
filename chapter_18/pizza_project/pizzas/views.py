8a59749480911f5769984cdbd457bc32cd254caf
Kerowyn202 <noellelaurinriley@gmail.com>
2024-05-26 19:38:46 -0700

    Changed accidental reference to learning logs back to pizzas

--- chapter_18/pizza_project/pizzas/views.py
+++ chapter_18/pizza_project/pizzas/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     """The home page for pizzas"""
-    return render(request, 'learning_logs/index.html')
+    return render(request, 'pizzas/index.html')
