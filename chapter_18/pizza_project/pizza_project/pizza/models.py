f209e581a6313f8c60bd26b704bf7cbb2442a7c5
Kristin <knhbjr@gmail.com>
2024-06-04 15:52:16 -0500

    Created Pizza model

--- chapter_18/pizza_project/pizza_project/pizza/models.py
+++ chapter_18/pizza_project/pizza_project/pizza/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# Pizza and toppings.
+
+class Pizza(models.Model):
+    """Class representing pizza."""
+
+    text = models.TextField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string."""
+
+        return self.text
