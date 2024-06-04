e394ee7f34e2b8d195b47bb10cf96e65a90d7405
Kristin <knhbjr@gmail.com>
2024-06-04 16:09:52 -0500

    Added app to django settings

--- chapter_18/pizza_project/pizza_project/pizza_project_dj/settings.py
+++ chapter_18/pizza_project/pizza_project/pizza_project_dj/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My app
+    'pizza',
+    # Default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
