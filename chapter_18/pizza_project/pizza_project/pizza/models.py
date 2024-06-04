acfdaccf943ca0f80fb45dab726f4c764d02695d
Kristin <knhbjr@gmail.com>
2024-06-04 16:18:48 -0500

    Added topping model

--- chapter_18/pizza_project/pizza_project/pizza/models.py
+++ chapter_18/pizza_project/pizza_project/pizza/models.py
@@ -13,3 +13,26 @@ class Pizza(models.Model):
         """Return a string."""
 
         return self.text
+
+
+class Topping(models.Model):
+    """Class representing toppings."""
+
+    topic = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        verbose_name_plural = 'toppings'
+
+    def __str__(self):
+        """Topping attributes."""
+
+        if len(str(self.text)) >= 50:
+
+            return f"{self.text[:50]}..."
+
+        else:
+            return self.text
+
