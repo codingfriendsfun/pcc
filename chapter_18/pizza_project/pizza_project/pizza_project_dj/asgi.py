89f99562269a4c4d4eaa3e617cc462c9c34d4f3f
Kristin <knhbjr@gmail.com>
2024-06-04 16:06:33 -0500

    Renamed directory for clarity

--- chapter_18/pizza_project/pizza_project/pizza_project_dj/asgi.py
+++ chapter_18/pizza_project/pizza_project/pizza_project_dj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pizza_project project.
+ASGI config for pizza_project_dj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza_project_dj.settings')
 
 application = get_asgi_application()
--- chapter_18/pizza_project/pizza_project/pizza_project_dj/urls.py
+++ chapter_18/pizza_project/pizza_project/pizza_project_dj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pizza_project project.
+URL configuration for pizza_project_dj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
