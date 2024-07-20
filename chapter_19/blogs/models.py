4bb8a5f59d27a05a9845cf73a0d9b5428233f53d
Kerowyn202 <noellelaurinriley@gmail.com>
2024-07-20 14:38:45 -0700

    Created and activated models

--- chapter_19/blogs/models.py
+++ chapter_19/blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    """Model for overall blog"""
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
