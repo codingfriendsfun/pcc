9c4cd69f7bee2762dcbc3febea3e31e5fef1eac4
Kerowyn202 <noellelaurinriley@gmail.com>
2024-07-20 14:55:39 -0700

    Added and migrated Entry model

--- chapter_19/blogs/models.py
+++ chapter_19/blogs/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 class Blog(models.Model):
     """Model for overall blog"""
-    text = models.TextField()
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a string representation of the model"""
         return self.text
+    
+
+class Entry(models.Model):
+    """Individual blog entry"""
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return simple string representing entry"""
+        return f"{self.text[:50]}..."
